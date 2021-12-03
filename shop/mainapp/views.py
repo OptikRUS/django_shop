9d797a29367a0bdf576ac4c5de0649582280e300
optikrus <optikrus61@gmail.com>
2021-12-03 14:33:15 +0300

    Выполнил задание ко 2 уроку

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
+with open('links.json', 'r', encoding='utf-8') as f:
+    links_menu = json.load(f)
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'Главная',
+    }
+    return render(request, 'mainapp/index.html', content)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    content = {
+        'title': 'Продукты',
+        'links_menu': links_menu,
+    }
+    return render(request, 'mainapp/products.html', content)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'title': 'контакты',
+    }
+    return render(request, 'mainapp/contact.html', content)
