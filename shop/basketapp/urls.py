8104fea2ff5b0cd78bcb91ecae876206232272ed
optikrus <optikrus61@gmail.com>
2021-12-23 00:07:19 +0300

    Реализовал механизм добавления товаров в корзину

--- shop/basketapp/urls.py
+++ shop/basketapp/urls.py
@@ -6,6 +6,6 @@ app_name = 'basketapp'
 
 urlpatterns = [
     path('', basketapp.index, name='index'),
-    path('add/<int:pk>/', basketapp.add, name='add'),
+    path('add/<int:product_pk>/', basketapp.add, name='add'),
     path('remove/<int:pk>/', basketapp.remove, name='remove'),
 ]
--- shop/basketapp/views.py
+++ shop/basketapp/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from basketapp.models import BasketItem
+from django.http import HttpResponseRedirect
 
 def index(request):
-    pass
-
-
-def add(request, pk):
     pass
     # basket = BasketItem.objects.filter(user=request.user)
     # basket = request.user.basketitem_set.all() # тоже самое
     # basket = request.user.basket.all() # если указан related_name в models
+
+
+def add(request, product_pk):
+    basket_item, _ = BasketItem.objects.get_or_create(
+        user=request.user,
+        product_id=product_pk,
+    )
+    basket_item.qty += 1
+    basket_item.save()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 def remove(request, pk):
